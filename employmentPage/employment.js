b18fc9c0ef9b0354ca0bf747449e319e5acb18a0
iLyhaha <gollpfd@gmail.com>
2025-06-19 13:04:05 +0300

    fix

--- employmentPage/employment.js
+++ employmentPage/employment.js
@@ -1,271 +1,253 @@
-// const screenWidth = window.screen.width;
+const screenWidth = window.screen.width;
 
-// const testimonial = [
-//   {
-//     text: "Обратился по рекомендации друга. Ребята всё чётко объяснили, помогли собрать документы и уже через два месяца я получил ВНЖ в Польше. Без лишней бюрократии и нервов. Спасибо за поддержку на каждом этапе!",
-//     img: "/images/our-clients/1.png",
-//     title: "Алексей Ковальчук , 35 лет",
-//     description: "ВНЖ в Польше за 2 месяца через бизнес-иммиграцию.",
-//   },
-//   {
-//     text: "Очень рад, что выбрал именно эту компанию. У меня был сложный случай, но специалисты нашли оптимальное решение и оформили всё легально. Теперь я живу и работаю в Словакии официально.",
-//     img: "/images/our-clients/2.png",
-//     title: "Игорь Демидов , 42 года",
-//     description: "ВНЖ в Польше за 2 месяца через бизнес-иммиграцию.",
-//   },
-//   {
-//     text: "Оформление ВНЖ в Чехии прошло быстро и без подводных камней. Особенно понравилось, что всегда были на связи и заранее предупреждали обо всех нюансах. Рекомендую всем, кто хочет уехать по-человечески.",
-//     img: "/images/our-clients/3.png",
-//     title: "Сергей Антонов , 29 лет",
-//     description: "ВНЖ в Польше за 2 месяца через бизнес-иммиграцию.",
-//   },
-//   // {
-//   //   text:"Мне помогли не только с документами, но и с открытием банковского счёта, жильём и переводами. Комплексный подход чувствуется. Сейчас уже обживаюсь в Румынии, всё благодаря вам.",
-//   //   img:'/images/our-clients/1.png',
-//   //   title:'Владимир Ткаченко , 50 лет',
-//   //   description:"ВНЖ в Польше за 2 месяца через бизнес-иммиграцию."
-//   // },
-//   // {
-//   //   text:"Честно говоря, не ожидал такого уровня сервиса. Приятно удивлён. Легализация в Венгрии прошла быстро, постоянно получал обновления по статусу дела. Спасибо команде за профессионализм и честность.",
-//   //   img:'/images/our-clients/1.png',
-//   //   title:'Максим Соловей , 31 год',
-//   //   description:"ВНЖ в Польше за 2 месяца через бизнес-иммиграцию."
-//   // },
-// ];
+const testimonial = [
+  {
+    name: "Omar, 41",
+    img: "images/real-clients/1.png",
+    text: "Got a warehouse job in Poland. Applied for a residence permit within 3 months.",
+  },
+  {
+    name: "James, 34",
+    img: "images/real-clients/2.png",
+    text: "Started a construction job in Hungary. Employer provided housing and support with residency.",
+  },
+  {
+    name: "Omar, 41",
+    img: "images/real-clients/1.png",
+    text: "Got a warehouse job in Poland. Applied for a residence permit within 3 months.",
+  },
+  {
+    name: "James, 34",
+    img: "images/real-clients/2.png",
+    text: "Started a construction job in Hungary. Employer provided housing and support with residency.",
+  },
+];
 
-// let SLIDES_PER_PAGETestimonial = 3;
-// if (screenWidth <= 1265) {
-//   SLIDES_PER_PAGETestimonial = 2;
-// }
-// if (screenWidth <= 850) {
-//   SLIDES_PER_PAGETestimonial = 1;
-// }
+let SLIDES_PER_PAGETestimonial = 2;
+if (screenWidth <= 1265) {
+  SLIDES_PER_PAGETestimonial = 2;
+}
+if (screenWidth <= 850) {
+  SLIDES_PER_PAGETestimonial = 1;
+}
 
-// let currentSlideIndexTestimonial = 0;
+let currentSlideIndexTestimonial = 0;
 
-// function initTestimonialSlider() {
-//   const slidesContainerTestimonial = document.querySelector(".testimonial");
-//   const buttonPrevTestimonial = document.querySelector(
-//     ".testimonial-control-prev"
-//   );
-//   const buttonNextTestimonial = document.querySelector(
-//     ".testimonial-control-next"
-//   );
-//   const indicatorsTestimonial = document.querySelector(
-//     ".testimonial-indicator"
-//   );
+function initTestimonialSlider() {
+  const slidesContainerTestimonial = document.querySelector(".testimonial");
+  const buttonPrevTestimonial = document.querySelector(
+    ".testimonial-control-prev"
+  );
+  const buttonNextTestimonial = document.querySelector(
+    ".testimonial-control-next"
+  );
+  const indicatorsTestimonial = document.querySelector(
+    ".testimonial-indicator"
+  );
 
-//   function createTestimonialBlock(person) {
-//     return `
-//       <div class="our-clients-swiper-card">
-//         <p class="our-clients-swiper-card-text">
-//           ${person.text}
-//         </p>
-//         <div class="our-clients-flex-line">
-//         <div class="our-clients-swiper-card-line"></div>
-//         <div class="our-clients-swiper-card-info">
-//           <img src="${person.img}" alt="">
-//           <div class="our-clients-swiper-card-info-text">
-//             <h4>${person.title}</h4>
-//             <p>${person.description}</p>
-//           </div>
-//         </div>
-//         </div>
-//       </div>
-//     `;
-//   }
+  function createTestimonialBlock(person) {
+    return `
+      <div class="real-clients-swiper-card">
+        <img src="${person.img}" alt="">
+        <div class="real-clients-swiper-card-text">
+            <h3 class="text-xl bold700" style="color: #E95404;text-transform: uppercase;">${person.name}</h3>
+            <h4 class="text-l">${person.text}</h4>
+        </div>
+      </div>
+    `;
+  }
 
-//   function updateSlidesTestimonial() {
-//     const totalSlides = Math.ceil(
-//       testimonial.length / SLIDES_PER_PAGETestimonial
-//     );
-//     const currentSlide = document.querySelector(".swiper-slide.active");
+  function updateSlidesTestimonial() {
+    const totalSlides = Math.ceil(
+      testimonial.length / SLIDES_PER_PAGETestimonial
+    );
+    const currentSlide = document.querySelector(".swiper-slide.active");
 
-//     // Create new slide
-//     const newSlide = document.createElement("div");
-//     newSlide.className = "swiper-slide";
+    // Create new slide
+    const newSlide = document.createElement("div");
+    newSlide.className = "swiper-slide";
 
-//     const startIdx = currentSlideIndexTestimonial * SLIDES_PER_PAGETestimonial;
-//     const endIdx = Math.min(
-//       startIdx + SLIDES_PER_PAGETestimonial,
-//       testimonial.length
-//     );
+    const startIdx = currentSlideIndexTestimonial * SLIDES_PER_PAGETestimonial;
+    const endIdx = Math.min(
+      startIdx + SLIDES_PER_PAGETestimonial,
+      testimonial.length
+    );
 
-//     const slideContent = testimonial
-//       .slice(startIdx, endIdx)
-//       .map((person) => createTestimonialBlock(person))
-//       .join("");
+    const slideContent = testimonial
+      .slice(startIdx, endIdx)
+      .map((person) => createTestimonialBlock(person))
+      .join("");
 
-//     newSlide.innerHTML = slideContent;
+    newSlide.innerHTML = slideContent;
 
-//     // Add new slide to container
-//     const slidesContainerTestimonial = document.querySelector(".swiper-slides");
+    // Add new slide to container
+    const slidesContainerTestimonial = document.querySelector(".swiper-slides");
 
-//     if (currentSlide) {
-//       // Fade out current slide
-//       currentSlide.style.opacity = "0";
-//       currentSlide.style.visibility = "hidden";
+    if (currentSlide) {
+      // Fade out current slide
+      currentSlide.style.opacity = "0";
+      currentSlide.style.visibility = "hidden";
 
-//       // Wait for fade out animation
-//       setTimeout(() => {
-//         currentSlide.remove();
-//         slidesContainerTestimonial.appendChild(newSlide);
+      // Wait for fade out animation
+      setTimeout(() => {
+        currentSlide.remove();
+        slidesContainerTestimonial.appendChild(newSlide);
 
-//         // Trigger reflow
-//         void newSlide.offsetWidth;
+        // Trigger reflow
+        void newSlide.offsetWidth;
 
-//         // Add active class to trigger fade in
-//         newSlide.classList.add("active");
-//       }, 200);
-//     } else {
-//       slidesContainerTestimonial.appendChild(newSlide);
-//       setTimeout(() => {
-//         newSlide.classList.add("active");
-//       }, 0);
-//     }
+        // Add active class to trigger fade in
+        newSlide.classList.add("active");
+      }, 200);
+    } else {
+      slidesContainerTestimonial.appendChild(newSlide);
+      setTimeout(() => {
+        newSlide.classList.add("active");
+      }, 0);
+    }
 
-//     updatePaginationTestimonial(totalSlides);
-//   }
+    updatePaginationTestimonial(totalSlides);
+  }
 
-//   function updatePaginationTestimonial(totalSlides) {
-//     indicatorsTestimonial.innerHTML = "";
+  function updatePaginationTestimonial(totalSlides) {
+    indicatorsTestimonial.innerHTML = "";
 
-//     if (totalSlides <= 5) {
-//       for (let i = 0; i < totalSlides; i++) {
-//         addPageButtonTestimonial(i);
-//       }
-//     } else {
-//       if (currentSlideIndexTestimonial > 2) {
-//         addPageButtonTestimonial(0);
-//         addEllipsisTestimonial();
-//       }
+    if (totalSlides <= 5) {
+      for (let i = 0; i < totalSlides; i++) {
+        addPageButtonTestimonial(i);
+      }
+    } else {
+      if (currentSlideIndexTestimonial > 2) {
+        addPageButtonTestimonial(0);
+        addEllipsisTestimonial();
+      }
 
-//       for (
-//         let i = Math.max(0, currentSlideIndexTestimonial - 1);
-//         i <= Math.min(totalSlides - 1, currentSlideIndexTestimonial + 1);
-//         i++
-//       ) {
-//         addPageButtonTestimonial(i);
-//       }
+      for (
+        let i = Math.max(0, currentSlideIndexTestimonial - 1);
+        i <= Math.min(totalSlides - 1, currentSlideIndexTestimonial + 1);
+        i++
+      ) {
+        addPageButtonTestimonial(i);
+      }
 
-//       if (currentSlideIndexTestimonial < totalSlides - 3) {
-//         addEllipsisTestimonial();
-//         addPageButtonTestimonial(totalSlides - 1);
-//       }
-//     }
-//   }
+      if (currentSlideIndexTestimonial < totalSlides - 3) {
+        addEllipsisTestimonial();
+        addPageButtonTestimonial(totalSlides - 1);
+      }
+    }
+  }
 
-//   function addPageButtonTestimonial(pageNum) {
-//     const button = document.createElement("div");
-//     button.className = `indicator-swiper${
-//       pageNum === currentSlideIndexTestimonial ? " active" : ""
-//     }`;
-//     button.dataset.testimonial = pageNum;
-//     button.textContent = pageNum + 1;
-//     indicatorsTestimonial.appendChild(button);
-//   }
+  function addPageButtonTestimonial(pageNum) {
+    const button = document.createElement("div");
+    button.className = `indicator-swiper${
+      pageNum === currentSlideIndexTestimonial ? " active" : ""
+    }`;
+    button.dataset.testimonial = pageNum;
+    button.textContent = pageNum + 1;
+    indicatorsTestimonial.appendChild(button);
+  }
 
-//   function addEllipsisTestimonial() {
-//     const span = document.createElement("span");
-//     span.textContent = "...";
-//     span.className = "pagination-ellipsis";
-//     indicatorsTestimonial.appendChild(span);
-//   }
+  function addEllipsisTestimonial() {
+    const span = document.createElement("span");
+    span.textContent = "...";
+    span.className = "pagination-ellipsis";
+    indicatorsTestimonial.appendChild(span);
+  }
 
-//   function nextSlideTestimonial() {
-//     const totalSlides = Math.ceil(
-//       testimonial.length / SLIDES_PER_PAGETestimonial
-//     );
-//     if (currentSlideIndexTestimonial < totalSlides - 1) {
-//       currentSlideIndexTestimonial++;
-//       updateSlidesTestimonial();
-//     }
-//   }
+  function nextSlideTestimonial() {
+    const totalSlides = Math.ceil(
+      testimonial.length / SLIDES_PER_PAGETestimonial
+    );
+    if (currentSlideIndexTestimonial < totalSlides - 1) {
+      currentSlideIndexTestimonial++;
+      updateSlidesTestimonial();
+    }
+  }
 
-//   function prevSlideTestimonial() {
-//     if (currentSlideIndexTestimonial > 0) {
-//       currentSlideIndexTestimonial--;
-//       updateSlidesTestimonial();
-//     }
-//   }
+  function prevSlideTestimonial() {
+    if (currentSlideIndexTestimonial > 0) {
+      currentSlideIndexTestimonial--;
+      updateSlidesTestimonial();
+    }
+  }
 
-//   // Touch events
-//   let startX, endX;
+  // Touch events
+  let startX, endX;
 
-//   function handleTouchStartTestimonial(event) {
-//     startX = event.touches[0].clientX;
-//   }
+  function handleTouchStartTestimonial(event) {
+    startX = event.touches[0].clientX;
+  }
 
-//   function handleTouchMoveTestimonial(event) {
-//     endX = event.touches[0].clientX;
-//   }
+  function handleTouchMoveTestimonial(event) {
+    endX = event.touches[0].clientX;
+  }
 
-//   function handleTouchEndTestimonial() {
-//     if (!endX) return;
+  function handleTouchEndTestimonial() {
+    if (!endX) return;
 
-//     const deltaX = endX - startX;
-//     if (Math.abs(deltaX) > 50) {
-//       if (deltaX < 0) {
-//         nextSlideTestimonial();
-//       } else {
-//         prevSlideTestimonial();
-//       }
-//     }
-//   }
+    const deltaX = endX - startX;
+    if (Math.abs(deltaX) > 50) {
+      if (deltaX < 0) {
+        nextSlideTestimonial();
+      } else {
+        prevSlideTestimonial();
+      }
+    }
+  }
 
-//   // Event Listeners
-//   buttonNextTestimonial.addEventListener("click", nextSlideTestimonial);
-//   buttonPrevTestimonial.addEventListener("click", prevSlideTestimonial);
+  // Event Listeners
+  buttonNextTestimonial.addEventListener("click", nextSlideTestimonial);
+  buttonPrevTestimonial.addEventListener("click", prevSlideTestimonial);
 
-//   indicatorsTestimonial.addEventListener("click", (event) => {
-//     const clickedIndicator = event.target;
-//     if (clickedIndicator.classList.contains("indicator-swiper")) {
-//       currentSlideIndexTestimonial = parseInt(
-//         clickedIndicator.dataset.testimonial,
-//         10
-//       );
-//       updateSlidesTestimonial();
-//     }
-//   });
+  indicatorsTestimonial.addEventListener("click", (event) => {
+    const clickedIndicator = event.target;
+    if (clickedIndicator.classList.contains("indicator-swiper")) {
+      currentSlideIndexTestimonial = parseInt(
+        clickedIndicator.dataset.testimonial,
+        10
+      );
+      updateSlidesTestimonial();
+    }
+  });
 
-//   slidesContainerTestimonial.addEventListener(
-//     "touchstart",
-//     handleTouchStartTestimonial
-//   );
-//   slidesContainerTestimonial.addEventListener(
-//     "touchmove",
-//     handleTouchMoveTestimonial
-//   );
-//   slidesContainerTestimonial.addEventListener(
-//     "touchend",
-//     handleTouchEndTestimonial
-//   );
+  slidesContainerTestimonial.addEventListener(
+    "touchstart",
+    handleTouchStartTestimonial
+  );
+  slidesContainerTestimonial.addEventListener(
+    "touchmove",
+    handleTouchMoveTestimonial
+  );
+  slidesContainerTestimonial.addEventListener(
+    "touchend",
+    handleTouchEndTestimonial
+  );
 
-//   // Initial render
-//   updateSlidesTestimonial();
-// }
+  // Initial render
+  updateSlidesTestimonial();
+}
 
-// document.addEventListener("DOMContentLoaded", initTestimonialSlider);
+document.addEventListener("DOMContentLoaded", initTestimonialSlider);
 
-// const AccordeonTopButton = document.querySelectorAll(
-//   ".accordeon-item-top-button"
-// );
+const AccordeonTopButton = document.querySelectorAll(
+  ".accordeon-item-top-button"
+);
 
-// AccordeonTopButton.forEach((el) => {
-//   el.addEventListener("click", () => {
-//     const content = el.nextElementSibling;
-//     const img = el.lastElementChild;
-//     img.classList.toggle("active");
+AccordeonTopButton.forEach((el) => {
+  el.addEventListener("click", () => {
+    const content = el.nextElementSibling;
+    const img = el.lastElementChild;
+    img.classList.toggle("active");
 
-//     if (content.style.maxHeight) {
-//       content.style.maxHeight = null;
-//       content.style.paddingTop = "0px";
-//     } else {
-//       content.style.paddingTop = "10px";
-//       content.style.maxHeight = content.scrollHeight + 10 + "px";
-//     }
-//   });
-// });
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+      content.style.paddingTop = "0px";
+    } else {
+      content.style.paddingTop = "10px";
+      content.style.maxHeight = content.scrollHeight + 10 + "px";
+    }
+  });
+});
 
 const burgerButton = document.querySelector(".custom-burger");
 const slidingMenu = document.querySelector(".sliding-menu");
